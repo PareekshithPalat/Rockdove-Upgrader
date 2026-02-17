398e95dd4394411f6149f976c365d443a73d8bc9
Lakshan Ganesan <lakapple05@gmail.com>
2026-02-17 22:09:21 +0530

    navbar change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -124,7 +124,7 @@ const Header: React.FC = () => {
               <img
                 src="/rockdove-logo.png"
                 alt="RockDove Logo"
-                className="h-9 w-auto"
+                className="h-24 w-auto object-contain transition-transform duration-300 hover:scale-110"
               />
             </Link>
 
@@ -349,7 +349,7 @@ const Header: React.FC = () => {
                 <img
                   src="/rockdove-logo.png"
                   alt="RockDove Logo"
-                  className="h-12 w-auto"
+                  className="h-28 w-auto"
                 />
                 <button
                   onClick={() => setMobileOpen(false)}
--- src/pages/MRO.tsx
+++ src/pages/MRO.tsx
@@ -40,16 +40,7 @@ const MRO: React.FC = () => {
       <div className="min-h-screen bg-black text-white">
         {/* Hero Section */}
         <section className="relative pt-32 pb-20 px-8 overflow-hidden">
-          {/* Background Glow */}
-          <div
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 pointer-events-none"
-            style={{ width: "55vw", height: "55vh" }}
-          >
-            <div
-              className="w-full h-full rounded-full bg-[#5cc6d0] opacity-20"
-              style={{ filter: "blur(120px)" }}
-            ></div>
-          </div>
+
           <div className="max-w-6xl mx-auto">
             {/* Animated title and icon */}
             <FadeInUp delay={200}>
@@ -203,8 +194,8 @@ const MRO: React.FC = () => {
 
           </div>
         </section>
-      </div>
-    </PageLayout>
+      </div >
+    </PageLayout >
   );
 };
 
